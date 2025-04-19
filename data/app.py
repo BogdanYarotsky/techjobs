95ef7306f2fdec46a278726f99a758d435593a84
byar <byarotsky@gmail.com>
2025-04-19 12:44:30 +0200

    first version of cleaned data

--- data/app.py
+++ data/app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 csv_file_path = '/Users/byar/Downloads/stack-overflow-developer-survey-2021/survey_results_public.csv'
-df = pd.read_csv(csv_file_path, low_memory=False)
-
+df = pd.read_csv(csv_file_path)
 
 cols_to_remove = [
     'ResponseId', 'US_State', 'UK_Country', 
@@ -12,7 +11,23 @@ cols_to_remove = [
     'NEWStuck', 'NEWSOSites', 'SOVisitFreq', 'SOAccount', 'SOPartFreq', 'SOComm', 'NEWOtherComms'
     ] + [col for col in df.columns if col.endswith('WantToWorkWith')]
 
-clean_df = df.drop(cols_to_remove, axis=1)
-print(clean_df.columns.tolist())
+df = df.drop(cols_to_remove, axis=1)
+df = df.dropna(subset=["ConvertedCompYearly", "YearsCodePro"])
 
+df['YearsCodePro'] = df['YearsCodePro'].replace({
+    'More than 50 years': 50,
+    'Less than 1 year': 0
+})
 
+df['YearsCodePro'] = pd.to_numeric(df['YearsCodePro'], errors='raise')
+
+df = df[
+    (df['MainBranch'] == 'I am a developer by profession') 
+    & (df['Employment'] == 'Employed full-time') 
+    & (df['ConvertedCompYearly'] > 0) 
+    & (df['YearsCodePro'] >= 0)
+    ]
+
+df = df.drop(['MainBranch', 'Employment'], axis=1)
+print(df.columns.to_list())
+print(len(df))
