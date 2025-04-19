404f2edaf2d0cce149426529c926d96673554363
byar <byarotsky@gmail.com>
2025-04-19 09:09:37 +0200

    wip - cleaning data for 2021

--- data/app.py
+++ data/app.py
@@ -1,9 +1,18 @@
 import pandas as pd
-print("hello")
-
-csv_file_path = '/Users/byar/Documents/survey_results_public_2021.csv'
-
+csv_file_path = '/Users/byar/Downloads/stack-overflow-developer-survey-2021/survey_results_public.csv'
 df = pd.read_csv(csv_file_path, low_memory=False)
 
+
+cols_to_remove = [
+    'ResponseId', 'US_State', 'UK_Country', 
+    'EdLevel', 'Age1stCode', 'LearnCode', 
+    'YearsCode', 'OrgSize', 'Currency', 
+    'CompTotal', 'CompFreq', 'Age', 'Gender', 'Trans', 'Sexuality', 
+    'Ethnicity', 'Accessibility', 'MentalHealth', 'SurveyLength', 'SurveyEase',
+    'NEWStuck', 'NEWSOSites', 'SOVisitFreq', 'SOAccount', 'SOPartFreq', 'SOComm', 'NEWOtherComms'
+    ] + [col for col in df.columns if col.endswith('WantToWorkWith')]
+
+clean_df = df.drop(cols_to_remove, axis=1)
+print(clean_df.columns.tolist())
 
 
